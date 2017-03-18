b2acddde0888c7becb349d69be63e4149877d9ba
raphaelsrodrigues <raph@raph-web.com>
2017-03-18 22:18:33 +0100

    looping json data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var cd = require('../costs_list.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express',
+      costdata: cd
+  });
 });
 
 module.exports = router;
