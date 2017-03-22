1187c0d71b1b17f5620d1e7131a28a669f6c7f38
raphaelsrodrigues <raph@raph-web.com>
2017-03-22 17:24:32 +0100

    mongodb setup 2

--- app.js
+++ app.js
@@ -39,8 +39,24 @@ MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
 
-    db.close();
+    insertDocuments(db, function() {
+        db.close();
 });
+
+var insertDocuments = function(db, callback) {
+    // Get the documents collection
+    var collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+        {a : 1}, {a : 2}, {a : 3}
+    ], function(err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+}
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
